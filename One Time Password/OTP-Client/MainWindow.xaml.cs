171d9461dcb263cf2aab56b36b9b2fd927e4376b
ManoliuSergiu <sermano14@gmail.com>
2019-03-30 18:55:10 +0200

    add client

---
+++ One Time Password/OTP-Client/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Net;
+using System.Net.Sockets;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OTP_Client
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+
+		public int secretW;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+			Load();
+		}
+
+		private void Load()
+		{
+			IPAddress IP = IPAddress.Parse("127.0.0.1");
+			TcpClient client = new TcpClient();
+			
+		}
+	}
+}
