f89b5387ce6f97df23f29eee0fb085fb9103549a
ManoliuSergiu <sermano14@gmail.com>
2019-03-30 19:47:49 +0200

    added one way function

--- One Time Password/OTP-Client/MainWindow.xaml.cs
+++ One Time Password/OTP-Client/MainWindow.xaml.cs
@@ -24,6 +24,10 @@ namespace OTP_Client
 	{
 
 		public int secretW;
+		public int ident;
+		public int t = 1000;
+
+		Random rnd = new Random();
 
 		public MainWindow()
 		{
@@ -35,7 +39,24 @@ namespace OTP_Client
 		{
 			IPAddress IP = IPAddress.Parse("127.0.0.1");
 			TcpClient client = new TcpClient();
+			client.Connect(IP, 80);
 			
+			secretW = rnd.Next(10000, 100000000);
+			byte[] data = Encoding.ASCII.GetBytes(secretW + "");
+
+			NetworkStream stream = client.GetStream();
+			stream.Write(data, 0, data.Length);
+
+		}
+		private int H(int input, int times)
+		{
+			int N = 10007 * 25471;
+			long aux = input;
+			for (int i = 0; i < times; i++)
+			{
+				aux = (aux * aux) % N;
+			}
+			return (int) aux;
 		}
 	}
 }
